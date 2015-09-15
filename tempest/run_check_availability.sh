8453c9569ba7ad9487bdd4defaaba3d8e8645e35
Raghvendra Maloo <rmaloo@yahoo.com>
2015-09-15 06:46:49 +0530

    migrating to single event with diff volume values

--- tempest/run_check_availability.sh
+++ tempest/run_check_availability.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 # run_check_availability.sh
 
+failed=0
+passed=0
+
+testrun_timeout=2m
+testfreq_sleep=1m
+samplecrt_timeout=1m
+
+function recover_evt {
+
+	 while [  $passed -gt 0 ]; do
+             timeout $samplecrt_timeout ceilometer sample-create --resource-id 0 --meter-name availability.status.event --meter-type gauge --meter-unit N/A --sample-volume 1 --direct True
+             let passed=passed-1 
+         done
+
+	while [  $failed -gt 0 ]; do
+             timeout $samplecrt_timeout ceilometer sample-create --resource-id 0 --meter-name availability.status.event --meter-type gauge --meter-unit N/A --sample-volume 0 --direct True
+             let failed=failed-1 
+         done
+
+}
+
 while :
 
 do
@@ -9,26 +30,40 @@ do
 
 	echo "rand value is $rand"
 
-	if [ $rand -gt 80 ]; then
+	if [ $rand -gt 60 ]; then
 	    
-	   ./run_tempest.sh tempest.api.failvol -N
+	   timeout $testrun_timeout ./run_tempest.sh tempest.api.failvol -N
 
 	else
 
-	   ./run_tempest.sh tempest.api.passvol -N
+	   timeout $testrun_timeout ./run_tempest.sh tempest.api.passvol -N
 
 	fi
 
         if [ $? -eq 0 ]; then
 	
-	    ceilometer sample-create --resource-id 0 --meter-name availability.status.passed --meter-type gauge --meter-unit N/A --sample-volume 0 --direct True
+	    timeout $samplecrt_timeout ceilometer sample-create --resource-id 0 --meter-name availability.status.event --meter-type gauge --meter-unit N/A --sample-volume 1 --direct True
+
+	    if [ $? -ne 0 ]; then
+	    	
+		let 'passed += 1'
+	    else
+		recover_evt
+	    fi
 
 	else
 	
-	    ceilometer sample-create --resource-id 0 --meter-name availability.status.failed --meter-type gauge --meter-unit N/A --sample-volume 0 --direct True
+	    timeout $samplecrt_timeout ceilometer sample-create --resource-id 0 --meter-name availability.status.event --meter-type gauge --meter-unit N/A --sample-volume 0 --direct True
+
+	    if [ $? -ne 0 ]; then
+	    	
+		let 'failed += 1'
+	    else
+		recover_evt
+	    fi
 	
 	fi
 
-	sleep 2m
+	sleep $testfreq_sleep
 
 done
