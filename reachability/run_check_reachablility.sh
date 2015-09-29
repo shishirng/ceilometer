add4ddbd8c64a2fd83e17eff85bd08984f73f3c3
Raghvendra Maloo <rmaloo@yahoo.com>
2015-09-29 15:10:20 +0530

    script to check reachability

--- reachability/run_check_reachablility.sh
+++ reachability/run_check_reachablility.sh
@@ -2,29 +2,37 @@
 # run_check_availability.sh
 
 ipaddress=100.113.160.55
-authtoken=34562e4889ff4897bb86f0f4268af940
-cinderendpoint=http://100.113.160.55:8776/v2/c2e7ea532e0442b991d4db3097bbabe3/volumes
+
+testfreq_sleep=1m
 
 while :
 
 do
 
-        curl -s -X POST http://$ipaddress:5000/v2.0/tokens -H "Content-Type: application/json" -d '{"auth": {"tenantName": "'"$OS_TENANT_NAME"'", "passwordCredentials": {"username": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"}}}' | python -m json.tool
+        authtoken=`curl -s -X POST http://$ipaddress:5000/v2.0/tokens -H "Content-Type: application/json" -d '{"auth": {"tenantName": "'"$OS_TENANT_NAME"'", "passwordCredentials": {"username": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"}}}' | python -m json.tool | jq .access.token.id | sed "s/\"//g"` 
 
-	curl -H "X-Auth-Token: $authtoken" "$cinderendpoint" | python -m json.tool
+tenant=`curl -s -X POST http://$ipaddress:5000/v2.0/tokens -H "Content-Type: application/json" -d '{"auth": {"tenantName": "'"$OS_TENANT_NAME"'", "passwordCredentials": {"username": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"}}}' | python -m json.tool | jq .access.token.tenant.id | sed "s/\"//g"`
+
+	echo -e "tenant=$tenant and authtok=$authtoken"
+
 
 	if [ $? -eq 0 ]; then
+
+		#curl -H "X-Auth-Token: $authtoken" "$cinderendpoint/volumes" | python -m json.tool > /dev/null
+		curl -H "X-Auth-Token: $authtoken" "http://$ipaddress:8776/v2/$tenant/volumes" | python -m json.tool > /dev/null
+
+		if [ $? -eq 0 ]; then
 	
-	    curl -g -i -X 'POST' "http://$ipaddress:8777/v2/meters/reachability.status.event?direct=True" -H 'User-Agent: ceilometerclient.openstack.common.apiclient' -H 'Content-Type: application/json' -H "X-Auth-Token: $authtoken" -d '[{"counter_type": "gauge", "counter_name": "reachability.status.event", "resource_id": "0", "counter_unit": "N/A", "counter_volume": "1.0"}]'
+		    curl -g -i -X 'POST' "http://$ipaddress:8777/v2/meters/reachability.status.event?direct=True" -H 'User-Agent: ceilometerclient.openstack.common.apiclient' -H 'Content-Type: application/json' -H "X-Auth-Token: $authtoken" -d '[{"counter_type": "gauge", "counter_name": "reachability.status.event", "resource_id": "0", "counter_unit": "N/A", "counter_volume": "1.0"}]' > /dev/null
 
 
-	else
+		else
 	
-	    curl -g -i -X 'POST' "http://$ipaddress:8777/v2/meters/reachability.status.event?direct=True" -H 'User-Agent: ceilometerclient.openstack.common.apiclient' -H 'Content-Type: application/json' -H "X-Auth-Token: $authtoken" -d '[{"counter_type": "gauge", "counter_name": "reachability.status.event", "resource_id": "0", "counter_unit": "N/A", "counter_volume": "0.0"}]'
+		    curl -g -i -X 'POST' "http://$ipaddress:8777/v2/meters/reachability.status.event?direct=True" -H 'User-Agent: ceilometerclient.openstack.common.apiclient' -H 'Content-Type: application/json' -H "X-Auth-Token: $authtoken" -d '[{"counter_type": "gauge", "counter_name": "reachability.status.event", "resource_id": "0", "counter_unit": "N/A", "counter_volume": "0.0"}]'
 
 	
+		fi
 	fi
 	
-	sleep 2m
-
+	sleep $testfreq_sleep
 done
