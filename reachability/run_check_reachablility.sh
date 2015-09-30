94b15c2bcf37379a15901497c67a5a548413ec75
Raghvendra Maloo <rmaloo@yahoo.com>
2015-09-30 18:09:17 +0530

    script for checking reachability with email failure notifications

--- reachability/run_check_reachablility.sh
+++ reachability/run_check_reachablility.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # run_check_availability.sh
 
+source ./query_sample_avg.sh
+
 function usage {
   echo "Usage: run_check_reachability <ipaddress>"
   echo ""
@@ -43,6 +45,16 @@ tenant=`curl -s -X POST http://$ipaddress:5000/v2.0/tokens -H "Content-Type: app
 		else
 	
 		    curl -g -i -X 'POST' "http://$ipaddress:8777/v2/meters/reachability.status.event?direct=True" -H 'User-Agent: ceilometerclient.openstack.common.apiclient' -H 'Content-Type: application/json' -H "X-Auth-Token: $authtoken" -d '[{"counter_type": "gauge", "counter_name": "reachability.status.event", "resource_id": "0", "counter_unit": "N/A", "counter_volume": "0.0"}]'
+
+		    compute_avg reachability.status.event 0 0 1 0 0 0  avg_returned_value ttc_returned_value
+			
+		    echo "To:raghvendra.maloo@ril.com;swami.reddy@ril.com" > /tmp/mail.txt
+		    echo "Subject:Reachability Test Failure" >> /tmp/mail.txt
+		    echo "Reachability Drop ALERT!! Reachability for last one hour is $avg_returned_value%" >> /tmp/mail.txt
+
+		    curl --url "smtps://smtp.gmail.com:465" --ssl-reqd   --mail-from "rjil.notify@gmail.com" --mail-rcpt "raghvendra.maloo@ril.com"   --upload-file /tmp/mail.txt --user "rjil.notify@gmail.com:cloud@123" --insecure
+
+		    rm -f /tmp/mail.txt
 
 	
 		fi
